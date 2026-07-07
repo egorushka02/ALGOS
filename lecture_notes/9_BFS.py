6ade26f1b77036ad6e9653ecbf42656c2f8f2723
Egor Ushakov <usakovegor2002@gmail.com>
2026-07-07 22:42:01 +0300

    Add implementation for Lowest Common Ancestor of a Binary Tree

--- lecture_notes/9_BFS.py
+++ lecture_notes/9_BFS.py
@@ -226,4 +226,43 @@ class Solution(object):
 103. Binary Tree Zigzag Level Order Traversal
 """
 
-        
+"""
+236. Lowest Common Ancestor of a Binary Tree
+"""
+# TODO
+# look at this again, it is not intuitive, need to understand it better
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+
+        if l and r:
+            return root
+        if l:
+            return l
+        if r:
+            return r
+
+        return None
+    
+# TODO
+"""
+117. Populating Next Right Pointers in Each Node II
+"""
