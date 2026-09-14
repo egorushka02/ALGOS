cb23ebd9e390e0ae3e2f260820a29b01a0e0f3ad
Egor Ushakov <usakovegor2002@gmail.com>
2026-09-14 19:45:57 +0300

    Add implementation for Edit Distance algorithm

--- lecture_notes/23_dynamic_programming_pt2.py
+++ lecture_notes/23_dynamic_programming_pt2.py
@@ -72,3 +72,57 @@ class Solution(object):
                 dp[i][j] = min(dp[i-1][j] + grid[i-1][j], dp[i][j-1] + grid[i][j-1])
         
         return dp[n-1][m-1] + grid[n-1][m-1]
+
+"""
+72. Edit Distance
+"""
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        def lev(a, b):
+            if len(b) == 0:
+                return len(a)
+            if len(a) == 0:
+                return len(b)
+            if a[0] == b[0]:
+                return lev(a[1:], b[1:])
+            return 1 + min(
+                lev(a[1:], b),
+                lev(a, b[1:]),
+                lev(a[1:], b[1:]),
+            )
+        return lev(word1, word2)
+
+# V2
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n = len(word1)
+        m = len(word2)
+
+        dp = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            dp[i][0] = dp[i-1][0] + 1
+        for j in range(1, m+1):
+            dp[0][j] = dp[0][j-1] + 1
+            
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(
+                        dp[i-1][j-1],
+                        dp[i-1][j],
+                        dp[i][j-1],
+                    )
+        return dp[n][m]
